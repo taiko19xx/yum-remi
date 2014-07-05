ffe3e3aaf5ab64041794e8e861fde686a49bc1ce
taiko19xx <taiko19xx@gmail.com>
2014-07-05 15:18:05 +0900

    Add "remi-php55" and "remi-php56" repositories.

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,26 @@ yum_repository "remi" do
 
   action      :create
 end
+
+# remi-php55 (only PHP 5.5 and extensions, also needs remi)
+yum_repository "remi-php55" do
+  description "Les RPM de remi de PHP 5.5 pour Enterprise Linux #{node['platform_version']} - $basearch"
+  mirrorlist  "http://rpms.famillecollet.com/enterprise/#{node['platform_version'].to_i}/php55/mirror"
+  enabled     false
+  gpgcheck    true
+  gpgkey      "http://rpms.famillecollet.com/RPM-GPG-KEY-remi"
+
+  action      :create
+end
+
+# remi-php56 (only PHP 5.6 and extensions, also needs remi, EXPERIMENTAL)
+yum_repository "remi-php56" do
+  description "Les RPM de remi de PHP 5.6 pour Enterprise Linux #{node['platform_version']} - $basearch"
+  mirrorlist  "http://rpms.famillecollet.com/enterprise/#{node['platform_version'].to_i}/php56/mirror"
+  enabled     false
+  gpgcheck    true
+  gpgkey      "http://rpms.famillecollet.com/RPM-GPG-KEY-remi"
+
+  action      :create
+end
+
